6bbf09e40c003502392c8ac4532feac9f3405d6d
Babita3 <babita.sima1996@gmail.com>
2022-10-22 19:31:53 +0530

    Welcome to Line comparison program

---
+++ Welcome_to_LineComparison.java
@@ -0,0 +1,9 @@
+package LineComparision;
+
+public class Welcome_to_LineComparison {
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to Line comparison Program.");
+    }
+
+}
